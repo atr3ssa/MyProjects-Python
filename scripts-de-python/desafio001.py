c777f8dd267a069e015bec54885b942a20203c0a
Andressa Rabêlo <dre.emy89@gmail.com>
2025-10-16 20:25:29 -0300

    desafio001 e 002

---
+++ scripts-de-python/desafio001.py
@@ -0,0 +1,2 @@
+nome = input("Qual o seu nome? ")
+print("Olá, ",nome,"! Eu me chamo Athena e é um prazer te conhecer!")
---
+++ scripts-de-python/desafio002.py
@@ -0,0 +1,8 @@
+print("===========DESAFIO 2===========")
+print("Olá, sou Athena e vou lhe fazer algumas perguntas!")
+dia = input("Em que dia você nasceu? ")
+mes = input("Em que mês você nasceu? ")
+ano = input("Em que ano você nasceu? ")
+print("Você nasceu no dia ",dia,"de ",mes,"de ",ano,". Correto?")
+
+
