639201ada79b8adcc2a0ce7bb407947a559a1c7f
Andressa Rabêlo <dre.emy89@gmail.com>
2025-10-16 20:50:52 -0300

    desafio003

---
+++ scripts-de-python/desafio003.py
@@ -0,0 +1,6 @@
+print("=========DESAFIO 3=========")
+numero1 = input("Primeiro número é: ")
+numero2 = input("Segundo número é: ")
+soma = (numero1 + numero2)
+print("A soma dos dois é: ",soma,"!")
+
