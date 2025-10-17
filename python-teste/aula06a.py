a72647839607e10c8eea17f9fb15a8c0352b6944
Andressa Rabêlo <dre.emy89@gmail.com>
2025-10-16 21:56:58 -0300

    aula006 e desafio004

---
+++ python-teste/aula06a.py
@@ -0,0 +1,5 @@
+n1 = int(input("Digite um número: ")) #convertendo tudo em inteiro
+n2 = int(input("Outro número: "))
+s = n1 + n2
+print(type(n1))
+print("A soma entre {} e {} é igual a: {}".format(n1, n2, s))
---
+++ python-teste/aula06b.py
@@ -0,0 +1,5 @@
+n = input("Digite algo: ")
+print(n.isnumeric()) # método para dizer se o que foi digitado é número ou não
+print(n.isalpha()) # método para dizer se o o que foi digitado é alfabético
+print(n.isalnum()) # método para dizer se o o que foi digitado é alfa numérico
+# métodos de teste de tipo
