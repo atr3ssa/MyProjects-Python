a226736f7ac9dd26b83a0b2f28a1ebd5f7219499
Andressa Rabêlo <dre.emy89@gmail.com>
2025-11-17 19:47:17 -0300

    ex07

---
+++ python-exercicios/ex007-Media_Aritmetica.py
@@ -0,0 +1,3 @@
+nota1= float(input("Digite a 1° Nota do Aluno:"))
+nota2= float(input("Digite a 2° Nota do Aluno:"))
+print("Maravilha! A Média entre a nota {} e a nota {}, é igual a: {:.1f}".format(nota1, nota2,((nota1+nota2)/2)))
