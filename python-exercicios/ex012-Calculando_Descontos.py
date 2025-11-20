77329f45c7bf733b5b04a9d4bb7e2383bc02fa46
Andressa Rabêlo <dre.emy89@gmail.com>
2025-11-20 10:49:41 -0300

    ex012

--- python-exercicios/ex012-Calculando_Descontos.py
+++ python-exercicios/ex012-Calculando_Descontos.py
@@ -1,4 +1,4 @@
 preco_produto = float(input("Qual é o preço do produto? R$"))
-desconto = (preco_produto*5)/100
+desconto = preco_produto*5/100
 preco_desconto =  preco_produto-desconto
 print("O produto que custava R${}, na promoção com desconto de 5% vai custar R${}".format(preco_produto,preco_desconto))
