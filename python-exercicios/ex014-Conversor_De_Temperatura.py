19637f70030868ecf07d732f5960815f16f6c63c
Andressa Rabêlo <dre.emy89@gmail.com>
2025-11-20 21:06:18 -0300

    ex014

---
+++ python-exercicios/ex014-Conversor_De_Temperatura.py
@@ -0,0 +1,3 @@
+temperatura_celsius = float(input("Qual a temperatura em °C: "))
+fahrenheit = (temperatura_celsius*1.8)+32
+print("A temperatura de {:.1f}°C corresponde a {:.1f}°F".format(temperatura_celsius,fahrenheit))
