433fa5f5425427d11c3e39a6c33f6bd9cc1a32ef
Andressa Rabêlo <dre.emy89@gmail.com>
2025-11-26 22:39:40 -0300

    aula08a

---
+++ python-teste/aula08a.py
@@ -0,0 +1,11 @@
+#from math import sqrt, floor
+#num = int(input("Digite um número: "))
+#raiz = sqrt(num) #math.(escolhe a funcionalidade(varavel que será base da operação))
+#print ("A raiz do numero {} é igual a {:.1f}".format(num,floor(raiz))) # ceil = arredondar para cima ( ou seja, arredondar a raiz para cima)
+
+#import random
+#num = random.randint(1,10) # para que o computador me diga um número aleatóro (random= float de 0 a 1 e randint= int e vc escolhe o )
+#print(num)
+
+import emoji
+print(emoji.emojize"")
